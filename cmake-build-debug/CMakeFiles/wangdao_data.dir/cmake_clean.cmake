1ac0dc89dce7f59dc45a1500ccf11e5957ae0df2
gong <1319147217@qq.com>
2022-05-15 16:31:08 +0800

    创建仓库

---
+++ cmake-build-debug/CMakeFiles/wangdao_data.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wangdao_data.dir/main.cpp.o"
+  "wangdao_data"
+  "wangdao_data.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wangdao_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
