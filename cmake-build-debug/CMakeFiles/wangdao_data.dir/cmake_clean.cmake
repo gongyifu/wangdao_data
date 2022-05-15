e2928ee19c4ce945506f3447063d96c3fa9e3c77
gong <1319147217@qq.com>
2022-05-15 16:52:22 +0800

    创建线性表文件

--- cmake-build-debug/CMakeFiles/wangdao_data.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/wangdao_data.dir/cmake_clean.cmake
@@ -1,5 +1,6 @@
 file(REMOVE_RECURSE
   "CMakeFiles/wangdao_data.dir/main.cpp.o"
+  "CMakeFiles/wangdao_data.dir/线性表/sqList.cpp.o"
   "wangdao_data"
   "wangdao_data.pdb"
 )
---
+++ 线性表/sqList.cpp
@@ -0,0 +1,15 @@
+//
+// Created by gongy on 2022/5/15.
+//
+#inclode <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAXSIZE 100
+
+/*定义顺序表*/
+typesef struct SqList{
+    int data[MAXSIZE];
+    int length;
+}SqList;
+
+/**/
